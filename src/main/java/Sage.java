74ad84668ea9ef8e7cf0f59f14780c7a1275bf2d
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-01-29 17:23:15 +0800

    Add support for echo

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Sage {
     public static void main(String[] args) {
         System.out.println("Hello there, Sage here.");
         System.out.println("How are you doing?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Echo user input
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
         System.out.println("Goodbye. Have a beautiful day.");
+        scanner.close();
     }
 }

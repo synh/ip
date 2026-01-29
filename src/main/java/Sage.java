8622191ea99c0e7125e6a93b97d9f2922a119035
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-01-29 17:05:01 +0800

    Add greeting

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -1,10 +1,7 @@
 public class Sage {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello there, Sage here.");
+        System.out.println("How are you doing?");
+        System.out.println("Goodbye. Have a beautiful day.");
     }
 }

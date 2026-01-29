e7fda2a5878923eb92e89eface4a9030813f2538
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-01-29 17:53:20 +0800

    Add support for list

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.lang.String;
+
 public class Sage {
     public static void main(String[] args) {
         System.out.println("Hello there, Sage here.");
         System.out.println("How are you doing?");
 
+        ArrayList<String> taskList = new ArrayList<String>();
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        // Echo user input
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                int index = 1;
+                for (String task : taskList) {
+                    System.out.println(index + ". " + task);
+                    index++;
+                }
+            } else {
+                taskList.add(input);
+                System.out.println("added: " + input);
+            }
             input = scanner.nextLine();
         }
 

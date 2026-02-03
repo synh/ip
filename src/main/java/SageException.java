cb91c386c5b9718a89ac14d9584bee3d3ff2b795
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-02-03 13:36:13 +0800

    Add SageException class

---
+++ src/main/java/SageException.java
@@ -0,0 +1,29 @@
+public class SageException extends Exception {
+    public SageException(String message) {
+        super(message);
+    }
+
+    /**
+     * Returns SageException with error message for invalid task number for Mark and Unmark commands.
+     * @return SageException
+     */
+    public static SageException invalidTaskNumber() {
+        return new SageException("That task doesn't exist, apparently. I guess you've completed a task outside of what you set out to do.\n");
+    }
+
+    /**
+     * Returns SageException with error message for Mark, Unmark, ToDo, Deadline, Event commands with invalid format.
+     * @return SageException
+     */
+    public static SageException invalidCommand(String command) {
+        return new SageException("That's an invalid " + command + ".\n");
+    }
+
+    /**
+     * Returns SageException with error message for remaining unknown errors.
+     * @return SageException
+     */
+    public static SageException unknownCommand() {
+        return new SageException("I'm afraid I didn't catch that.\n");
+    }
+}

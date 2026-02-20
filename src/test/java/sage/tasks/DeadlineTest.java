e7ba99eeef56ad866a0ae3ded9c4248daf5be2d3
Quek Syn Hui <56148373+synh@users.noreply.github.com>
2026-02-20 16:31:58 +0800

    Enhance DeadlineTest.java

--- src/test/java/sage/tasks/DeadlineTest.java
+++ src/test/java/sage/tasks/DeadlineTest.java
@@ -9,7 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
     @Test
     public void testStringConversion() {
-        Deadline deadline = new Deadline("buy books", LocalDate.parse("2026-12-31"));
-        assertEquals("[D][ ] buy books (by: 31 Dec 2026)", deadline.toString());
+        Deadline deadline1 = new Deadline("buy books", LocalDate.parse("2026-12-31"));
+        assertEquals("[D][ ] buy books (by: 31 Dec 2026)", deadline1.toString());
+
+        Deadline deadline2 = new Deadline("buy more books", LocalDate.parse("2026-02-28"));
+        assertEquals("[D][ ] buy more books (by: 28 Feb 2026)", deadline2.toString());
     }
 }
